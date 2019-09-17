3954786e3cb2e3b942402b71158d90cc1d774a41
Derek Miranda <derek.miranda@ff0000.com>
2019-09-16 17:57:44 -0700

    chore: C2.0 BT redist

--- src/1-build/common/js/EndFrame/postmarkups/index.js
+++ src/1-build/common/js/EndFrame/postmarkups/index.js
@@ -1,51 +1,58 @@
-import { createSideBySidePostMarkups } from './horizontalSideBySide.js'
-import horizontalStacked from './horizontalStacked.js'
-import { Align } from 'ad-view'
-import { ObjectUtils } from 'ad-utils'
+import { createSideBySidePostMarkups } from "./horizontalSideBySide.js";
+import horizontalStacked from "./horizontalStacked.js";
+import { Align } from "ad-view";
+import { ObjectUtils } from "ad-utils";
 
 const {
-	leftPostMarkup,
-	offCenterLeftPostMarkup,
-	centerPostMarkup,
-	offCenterRightPostMarkup,
-	rightPostMarkup
+  leftPostMarkup,
+  offCenterLeftPostMarkup,
+  centerPostMarkup,
+  offCenterRightPostMarkup,
+  rightPostMarkup
 } = createSideBySidePostMarkups({
-	defaultArgs: {
-		ctaLogoOffset: 25,
-		headlineFontSize: 18,
-		headlineLockupOffset: 20
-	},
-	yAlign: {
-		type: Align.BOTTOM,
-		offset: -26
-	},
-	layoutXAligns: {
-		leftXAlign: {
-			type: Align.LEFT,
-			against: 84
-		},
-		offCenterLeftXAlign: {
-			type: Align.LEFT,
-			against: 246
-		},
-		centerXAlign: Align.CENTER,
-		offCenterRightXAlign: {
-			type: Align.RIGHT,
-			offset: -246
-		},
-		rightXAlign: {
-			type: Align.RIGHT,
-			offset: -84
-		}
-	}
-})
+  defaultArgs: {
+    ctaLogoOffset: 25,
+    headlineFontSize: 18,
+    headlineLockupOffset: 20
+  },
+  yAlign: {
+    type: Align.BOTTOM,
+    offset: -26
+  },
+  layoutXAligns: {
+    leftXAlign: {
+      type: Align.LEFT,
+      against: 84
+    },
+    offCenterLeftXAlign: {
+      type: Align.LEFT,
+      against: 246
+    },
+    centerXAlign: Align.CENTER,
+    offCenterRightXAlign: {
+      type: Align.RIGHT,
+      offset: -246
+    },
+    rightXAlign: {
+      type: Align.RIGHT,
+      offset: -84
+    }
+  }
+});
 
 function stackedPostMarkup() {
-	horizontalStacked({
-		brandingLockupOffset: 20,
-		brandingLockupRightPadding: 32,
-		headlineFontSize: 18
-	})
+  horizontalStacked({
+    brandingLockupOffset: 20,
+    brandingLockupRightPadding: 32,
+    headlineFontSize: 18
+  });
 }
 
-export { leftPostMarkup, offCenterLeftPostMarkup, centerPostMarkup, offCenterRightPostMarkup, rightPostMarkup, stackedPostMarkup }
+export {
+  leftPostMarkup,
+  offCenterLeftPostMarkup,
+  centerPostMarkup,
+  offCenterRightPostMarkup,
+  rightPostMarkup,
+  stackedPostMarkup
+};
