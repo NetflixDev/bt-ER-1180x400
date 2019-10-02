f0acc2e96eed804233b22421bb9682fa9741a6df
Derek Miranda <derek.miranda@ff0000.com>
2019-10-02 12:12:13 -0700

    Applies ratings bug FPO
    - as well as intro clickthru fix
    - redist

--- dist/3-traffic/default/1180x400/creative.js
+++ dist/3-traffic/default/1180x400/creative.js
@@ -1,3 +1,12 @@
+// how long zoom animates for (in seconds)
+var ZOOM_DURATION = 1.7;
+
+// how much to scale the keyart intro frame
+var ZOOM_AMOUNT = 5;
+
+// how long fade-in takes (in seconds)
+var FADE_DURATION = 1.5;
+
 var Creative = function() {
   this.init = function() {
     console.log("Creative.init()");
@@ -9,7 +18,7 @@ var Creative = function() {
     var delay = 0;
     TweenLite.fromTo(
       View.endFrame,
-      1.5,
+      FADE_DURATION,
       { alpha: 0 },
       { alpha: 1, delay: delay }
     );
@@ -20,8 +29,6 @@ var Creative = function() {
   };
 };
 
-// how long zoom animates for (in seconds)
-Creative.zoomDuration = 1.7;
-
-// how much to scale the keyart intro frame
-Creative.zoomAmount = 5;
+// attaching to Creative class since container looks there for intro zoom properties
+Creative.zoomDuration = ZOOM_DURATION;
+Creative.zoomAmount = ZOOM_AMOUNT;
